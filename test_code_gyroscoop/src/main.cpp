c00a6a7d3bc8e36cef3d0138173bd6a48dd6409d
RelentlessRDS <RelentlessRDS@github.com>
2024-04-11 14:10:45 +0200

    more commit fixes

--- test_code_gyroscoop/src/main.cpp
+++ test_code_gyroscoop/src/main.cpp
@@ -8,5 +8,5 @@ void setup(){
 
 void loop() {
     Serial.println("roll: " + String(mpu.getAngle(1),0) + " pitch: " + String(mpu.getAngle(2),0));
-    delay(1); //Delay mag niet super laag anders kan hij niet meten en loopt hij vast.
+    delay(1);
 }
