fee33111cfa9617fd57c01fa10369ff2ea690330
RelentlessRDS <RelentlessRDS@github.com>
2024-06-18 11:01:57 +0200

    namespace addition

---
+++ test_code_gyroscoop/include/TravelControl.hpp
@@ -0,0 +1,15 @@
+#ifndef R2D2_TRAVELCONTROL_HPP
+#define R2D2_TRAVELCONTROL_HPP
+
+namespace asn {
+
+class  {
+public:
+    ();
+
+private:
+    
+};
+
+}  // namespace 
+#endif  // R2D2_TRAVELCONTROL_HPP
