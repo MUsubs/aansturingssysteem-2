9f3431140f76991172639b0b6a7fb19007be5e7d
RelentlessRDS <RelentlessRDS@github.com>
2024-06-18 11:37:00 +0200

    namespace formatting

--- test_code_gyroscoop/include/mpu6050.hpp
+++ test_code_gyroscoop/include/mpu6050.hpp
@@ -7,42 +7,47 @@
 #include <VarSpeedServo.h>
 #include "Kalman.h"
 
-class Mpu6050
+namespace asn
 {
-public:
-    Mpu6050(VarSpeedServo &my_servo, MPU6050 &mpu, Kalman &kalmanFilter);
-    float PID();
-    float getServo_pos();
-    float getSetpoint();
-    void setSetpoint(float s);
-    float highPassFilter(float current_value, float previous_value);
-    void setGyroUp();
-    float getCurrent_z();
-    void Move();
-    void kalman();
-    float mean();
 
-private:
-    VarSpeedServo &my_servo;
-    MPU6050 &mpu;
-    Kalman &kalmanFilter;
-    float servo_pos = 0.0;
-    float pos_prev = 0;
-    float previous_z = 0.0;
-    const float alpha = 0.8;
-    double setpoint = 0.0;
-    double error = 0.0;
-    double error_sum = 0.0;
-    double error_prev = 0.0;
-    double error_div = 0.0;
-    const double kp = 0.725;
-    const double ki = 1.02;
-    const double kd = 0.01;
-    const double dt = 0.1;
-    unsigned long current_time = 0;
-    float prev_time = 0;
-    float filtered_angle = 0;
-    int int_count = 0;
-};
+    class Mpu6050
+    {
+    public:
+        Mpu6050(VarSpeedServo &my_servo, MPU6050 &mpu, Kalman &kalmanFilter);
+        float PID();
+        float getServo_pos();
+        float getSetpoint();
+        void setSetpoint(float s);
+        float highPassFilter(float current_value, float previous_value);
+        void setGyroUp();
+        float getCurrent_z();
+        void Move();
+        void kalman();
+        float mean();
+
+    private:
+        VarSpeedServo &my_servo;
+        MPU6050 &mpu;
+        Kalman &kalmanFilter;
+        float servo_pos = 0.0;
+        float pos_prev = 0;
+        float previous_z = 0.0;
+        const float alpha = 0.8;
+        double setpoint = 0.0;
+        double error = 0.0;
+        double error_sum = 0.0;
+        double error_prev = 0.0;
+        double error_div = 0.0;
+        const double kp = 0.725;
+        const double ki = 1.02;
+        const double kd = 0.01;
+        const double dt = 0.1;
+        unsigned long current_time = 0;
+        float prev_time = 0;
+        float filtered_angle = 0;
+        int int_count = 0;
+    };
+
+}
 
 #endif // R2D2_MPU6050_HPP
--- test_code_gyroscoop/src/mpu6050.cpp
+++ test_code_gyroscoop/src/mpu6050.cpp
@@ -1,34 +1,35 @@
 #include "mpu6050.hpp"
 
+asn::Mpu6050::Mpu6050( VarSpeedServo& my_servo, MPU6050& mpu,
+                       Kalman& kalmanFilter ) :
+    my_servo( my_servo ), mpu( mpu ), kalmanFilter( kalmanFilter ) {
+}
 
-Mpu6050::Mpu6050( VarSpeedServo& my_servo, MPU6050& mpu, Kalman& kalmanFilter  )
-    : my_servo( my_servo ), mpu( mpu ), kalmanFilter( kalmanFilter ) {}
-
-float Mpu6050::highPassFilter( float current_value, float previous_value ) {
+float asn::Mpu6050::highPassFilter( float current_value,
+                                    float previous_value ) {
     return alpha * ( previous_value + current_value - alpha * previous_value );
 }
 
-void Mpu6050::setGyroUp(){
+void asn::Mpu6050::setGyroUp() {
     Wire.begin();
     my_servo.attach( 9 );
     byte status = mpu.begin();
     delay( 1000 );
     mpu.calcOffsets();
-    kalmanFilter.setAngle(0.0f);
-    kalmanFilter.setQangle(0.001f);
-    kalmanFilter.setQbias(0.0067f);
-    kalmanFilter.setRmeasure(0.075f);
+    kalmanFilter.setAngle( 0.0f );
+    kalmanFilter.setQangle( 0.001f );
+    kalmanFilter.setQbias( 0.0067f );
+    kalmanFilter.setRmeasure( 0.075f );
     prevTime = millis();
-
 }
 
-float Mpu6050::PID(){
+float asn::Mpu6050::PID() {
     mpu.update();
     float gyro_z = mpu.getAngleZ();
     float current_z = highPassFilter( gyro_z, previous_z );
 
     error = setpoint - current_z;
-    error_sum += error*dt;
+    error_sum += error * dt;
     error_div = ( error - error_prev ) / dt;
     servo_pos = ( kp * error + ki * error_sum + kd * error_div );
     error_prev = error;
@@ -39,29 +40,28 @@ float Mpu6050::PID(){
     return servo_pos;
 }
 
-float Mpu6050::getSetpoint(){
-  return setpoint;
+float asn::Mpu6050::getSetpoint() {
+    return setpoint;
 }
 
-float Mpu6050::getServo_pos(){
-  return servo_pos;
+float asn::Mpu6050::getServo_pos() {
+    return servo_pos;
 }
 
-void Mpu6050::setSetpoint( float s ){
-  setpoint = s;
+void asn::Mpu6050::setSetpoint( float s ) {
+    setpoint = s;
 }
 
-float Mpu6050::getCurrent_z(){
-  mpu.update();
-  return mpu.getAngleZ();
+float asn::Mpu6050::getCurrent_z() {
+    mpu.update();
+    return mpu.getAngleZ();
 }
 
-void Mpu6050::kalman(){
-
+void asn::Mpu6050::kalman() {
     current_time = millis();
 
-    servo_pos = kalmanFilter.getAngle(mpu.getAngleZ(), mpu.getAccZ(), (current_time - prev_time) / 1000);
+    servo_pos = kalmanFilter.getAngle( mpu.getAngleZ(), mpu.getAccZ(),
+                                       ( current_time - prev_time ) / 1000 );
 
     prev_time = current_time;
-
 }
