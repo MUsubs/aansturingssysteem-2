c5ecb7f1859781338ed13be6bf3eb141d6fdbcb9
RelentlessRDS <RelentlessRDS@github.com>
2024-06-18 11:50:11 +0200

    angle calculation as comment

--- test_code_gyroscoop/src/TravelControl.cpp
+++ test_code_gyroscoop/src/TravelControl.cpp
@@ -16,5 +16,7 @@ void asn::TravelControl::newDest() {
 }
 
 void asn::TravelControl::updateCurPos() {
-    
+
 }
+
+// angle = cos^-1((a_x * b_x) + (a_z * b_z) / (sqrt((a_x)^2 + (a_z)^2) * sqrt((b_x)^2 + (b_z)^2)))
